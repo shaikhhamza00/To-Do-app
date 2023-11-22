ea61c4d36bfab9153ca9f9e3a2bae296e1695a6e
shaikhhamza00 <mhdhamza.shaikh@gmail.com>
2023-11-22 21:24:38 +0500

    Update demo.kt

--- app/src/main/java/com/hamza_dev/todoapp/demo.kt
+++ app/src/main/java/com/hamza_dev/todoapp/demo.kt
@@ -15,6 +15,34 @@ class demo : AppCompatActivity() {
     private lateinit var addTaskButton: FloatingActionButton
     private lateinit var recyclerView: RecyclerView
     private lateinit var taskAdapter: TaskAdapter
-    
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize views
+        addTaskButton = findViewById(R.id.addTaskButton)
+        recyclerView = findViewById(R.id.recyclerView)
+
+        // Initialize RecyclerView and TaskAdapter
+        taskAdapter = TaskAdapter(mutableListOf(), object : TaskAdapter.OnItemClickListener {
+            override fun onItemClick(task: TaskAdapter.Task) {
+                showTaskDetailsBottomSheet(task)
+            }
+
+            override fun onCheckBoxClick(task: TaskAdapter.Task, isChecked: Boolean) {
+                // Handle checkbox click if needed
+            }
+        })
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = taskAdapter
+
+        // Set click listener for the addTaskButton
+        addTaskButton.setOnClickListener {
+            showAddTaskBottomSheet()
+        }
+    }
 
 }
